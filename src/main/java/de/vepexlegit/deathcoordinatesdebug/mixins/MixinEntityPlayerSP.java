e0e51f75f294f03a8c47dd2d7c32e0f42488ed14
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-06 12:30:10 +0400

    Create MixinEntityPlayerSP.java

---
+++ src/main/java/de/vepexlegit/deathcoordinatesdebug/mixins/MixinEntityPlayerSP.java
@@ -0,0 +1,38 @@
+package de.vepexlegit.deathcoordinatesdebug.mixins;
+
+import de.vepexlegit.deathcoordinatesdebug.DeathCoordinates;
+import de.vepexlegit.deathcoordinatesdebug.command.DeathCoordinatesCommand;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.GuiGameOver;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.client.ClientCommandHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityPlayerSP.class)
+public class MixinEntityPlayerSP {
+    @Inject(method = "onUpdate", at = @At("RETURN"))
+    private void startGame(final CallbackInfo ci) {
+        ClientCommandHandler.instance.registerCommand(new DeathCoordinatesCommand());
+    }
+
+    @Shadow private Minecraft mc = Minecraft.getMinecraft();
+
+    @Inject(method = "onUpdate", at = @At("HEAD"))
+    private void onUpdate(final CallbackInfo ci) {
+        if (DeathCoordinates.INSTANCE.isEnabled()) {
+            if (mc.thePlayer.getHealth() < 1.0f && mc.currentScreen instanceof GuiGameOver) {
+                int x = mc.thePlayer.getPosition().getX();
+                int y = mc.thePlayer.getPosition().getY();
+                int z = mc.thePlayer.getPosition().getZ();
+                if (mc.thePlayer.deathTime < 1) {
+                    mc.thePlayer.addChatMessage(new ChatComponentText("Death Coordinates: X: " + x + " Y: " + y + " Z: " + z));
+                }
+            }
+        }
+    }
+}
